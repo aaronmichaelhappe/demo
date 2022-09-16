5a40b6885045ef185e0c496db414c3f3d3d22172
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-09-16 03:09:14 -0700

    mostly cypress e2e setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  fixturesFolder: "tests/e2e/fixtures",
-  screenshotsFolder: "tests/e2e/screenshots",
-  videosFolder: "tests/e2e/videos",
+  projectId: "4wsx5i",
 
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./tests/e2e/plugins/index.js")(on, config);
+      // implement node event listeners here
     },
-    specPattern: "tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "tests/e2e/support/index.js",
   },
 
   component: {
