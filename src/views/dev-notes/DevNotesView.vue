7bd57efb03aa615c388e8da8f07e8cd8c06ca5b4
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-09-17 07:58:46 -0700

    new page for project notes and ideas

---
+++ src/views/dev-notes/DevNotesView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dev-notes-view container ml-4">
+    <h1 class="text-4xl">Ideas and docs and code organization rules</h1>
+    <div>
+      <h3 class="text-2xl">code org ideas</h3>
+      <ul class="ml-2">
+        <li>how to organize stores -- order?</li>
+        <ul class="ml-8 list-disc">
+          <li>refs first</li>
+          <li>computed</li>
+          <li>methods</li>
+        </ul>
+      </ul>
+      <ul class="ml-2">
+        <li>How to handle layout?</li>
+        <ul class="ml-8 list-disc">
+          <li>Use repeated component when able.</li>
+          <li>look into how to create more complicated classes in tailwind</li>
+          <li>
+            think about adding web components to project. for repeated elements
+            or layout maybe?
+            <ul class="ml-8 list-disc">
+              <li class="italic">lit or stencil?</li>
+            </ul>
+          </li>
+        </ul>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+// book pages
+// have many incomplent books to start
+// can edit author, editor, author 2
+// credits -> just do lorem ipsum for now
+// awards -> name and date for now
+// descriptions -> lorum ipsum for now
+</script>
