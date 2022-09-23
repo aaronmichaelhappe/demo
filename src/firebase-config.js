c76258ac6d0a6dbfca747e7d26fad991698ea59d
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-09-22 21:33:03 -0700

    firebase files

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyC73o8fshu40eTkxnpzs0Ra2aoD-7Z-qd0",
+  authDomain: "happe-books-8481b.firebaseapp.com",
+  projectId: "happe-books",
+  storageBucket: "happe-books.appspot.com",
+  messagingSenderId: "788070773248",
+  appId: "1:788070773248:web:d14e5bce55b2d7b3931ee5",
+};
