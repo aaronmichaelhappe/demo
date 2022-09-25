f5b1d851aa0963ee36829b75d19e6890884ec002
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-09-25 11:57:51 -0700

    minor path bug

--- src/views/flyers/partials/FlyersMainPart.vue
+++ src/views/flyers/partials/FlyersMainPart.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="flyer-main-part">
-    // TODO eventually move to its own element
-    <h1
-      class="mb-6 bg-gradient-to-r from-green-400 to-blue-400 bg-clip-text text-8xl font-extrabold leading-[120%] text-transparent"
-    >
-      Flyer Designer
-    </h1>
+    <AmhHeroH1></AmhHeroH1>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import AmhHeroH1 from "@/elements/AmhHeroH1.vue";
+</script>
