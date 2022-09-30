95abc3daf8c463e88bfbe37525ef53b2cfea161b
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-09-30 16:50:33 -0700

    remove test temp

--- src/views/books/tests/unit/BooksView.spec.js
+++ src/views/books/tests/unit/BooksView.spec.js
@@ -15,20 +15,3 @@ describe("BooksView.vue", () => {
     expect(books.length).toBe(10);
   });
 });
-
-// test("should have 10 books", async () => {
-//   // The render method returns a collection of utilities to query your component.
-//   const { getByText } = render(Component);
-
-//   // getByText returns the first matching node for the provided text, and
-//   // throws an error if no elements match or if more than one match is found.
-//   getByText("Times clicked: 0");
-
-//   const button = getByText("increment");
-
-//   // Dispatch a native click event to our button element.
-//   await fireEvent.click(button);
-//   await fireEvent.click(button);
-
-//   getByText("Times clicked: 2");
-// });
